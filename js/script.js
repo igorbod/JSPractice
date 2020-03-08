e4ac78aa76666ada07425f168892db6eafed8b8d
Igor <igorbod.webdev@gmail.com>
2020-03-08 18:04:10 +0700

    add open modal script to all nedeed buttons

--- js/script.js
+++ js/script.js
@@ -88,18 +88,27 @@ window.addEventListener('DOMContentLoaded', function() {
 
   // Modal
   let more = document.querySelector('.more'),
+      descrBtn = document.querySelectorAll('.description-btn'),
       overlay = document.querySelector('.overlay'),
       close = document.querySelector('.popup-close');
 
-  more.addEventListener('click', function() {
+  
+  function openModal() {
     overlay.style.display = 'block';
     this.classList.add('more-splash');
     document.body.style.overflow = 'hidden';
+  }
+  
+  function closeModal() {
+    overlay.style.display = 'none';
+    this.classList.remove('more-splash');
+    document.body.style.overflow = '';
+  }
+
+  more.addEventListener('click', openModal);
+  descrBtn.forEach(function(item) {
+    item.addEventListener('click', openModal);
   });
 
-  close.addEventListener('click', function() {
-    overlay.style.display = 'none';
-    more.classList.remove('more-splash');
-    document.body.style.overflow = '';
-  });
+  close.addEventListener('click', closeModal);
 });
